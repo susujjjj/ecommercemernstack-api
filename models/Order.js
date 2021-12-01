f7d9ea702a69d29a1af0b01c8229a8c6adb77ee4
sb <chloejjjj@naver.com>
2021-12-01 16:22:04 +0900

    auth , register post api 생성

--- models/Order.js
+++ models/Order.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username: { type: String , required: true, unique: true },
-    email: { type: String, required: true,  unique: true },
-    password: { ype: String, required: true },
-    isAdmin: {
-        type: boolean,
-        default: false,
+const OrderSchema = new mongoose.Schema(
+    {
+    userId: {type: String, required: true},
+    products: [
+        {
+            productId: {
+                type: String,
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            },
+        },
+    ],
+    amount: { type: Number , required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
     },
-    createdAt:Date.now()
-})
+    {timestamps: true},
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
