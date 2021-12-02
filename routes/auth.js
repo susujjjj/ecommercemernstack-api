7514f5af3c49682ade60f5a5973373d8b276067f
sb <chloejjjj@naver.com>
2021-12-03 03:24:09 +0900

    register에 가입할 유저정보 500error 해결 및, password 암호화함 (CryptopJS)

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User")
+const CryptoJS = require("crypto-js")
 
 //REGISTER
 router.post("/register", async (req, res) => {
-    const newUser= new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(
+        req.body.password, 
+        process.env.PASS_SEC
+      ).toString(), //PASS_SEC는 .env에 설정해줌 
     });
  
     try {
