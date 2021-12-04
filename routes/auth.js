d5a4b5cb00387fd94c3eae627a71eb74def64b3b
sb <chloejjjj@naver.com>
2021-12-04 19:11:01 +0900

    login api 생성

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User")
 const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -21,4 +22,49 @@ router.post("/register", async (req, res) => {
     }
 })
 
+//LOGIN
+router.post("/login", async(req, res) => {
+    try {
+      const user = await User.findOne({username: req.body.username});
+      !user && res.status(401).json("Wrong credentials!")
+      const hashedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.PASS_SEC
+      );
+      const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+      Originalpassword !== req.body.password && 
+        res.status(401).json("Wrong credentials!");
+
+        const accessToken = jwt.sign({
+            id: user._id, 
+            isAdmin: user.isAdmin,
+
+            }, 
+            process.env.JWT_SEC,
+            {expiresIn: "3d"}
+        )
+
+        const {password, ...others} = user._doc;  
+
+        // res.status(200).json(user);
+        res.status(200).json({...others, accessToken});
+      } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
+
+// {
+//     "others": {
+//         "_id": "61a90e6724851c6325f16731",
+//         "username": "nerd",
+//         "email": "nerd@gmail.com",
+//         "isAdmin": false,
+//         "createdAt": "2021-12-02T18:20:23.992Z",
+//         "updatedAt": "2021-12-02T18:20:23.992Z",
+//         "__v": 0
+//     },
+//     "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYTkwZTY3MjQ4NTFjNjMyNWYxNjczMSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2Mzg2MTEwMDEsImV4cCI6MTYzODg3MDIwMX0.l2v4XRwaZ9Jpg52e0AiT0IQZ-FhrOaQU2O0OqZ_9DRw"
+// }
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,36 @@
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-    res.send("user test is successfull");
-});
+// router.get("/usertest", (req, res) => {
+//     res.send("user test is successfull");
+// });
 
-router.post("/userposttest", (req, res) => {
-  const username = req.body.username;
-  res.send("your username is: " + username); 
+// router.post("/userposttest", (req, res) => {
+//   const username = req.body.username;
+//   res.send("your username is: " + username); 
+// });
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
